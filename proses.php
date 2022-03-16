76bcc4776a2f050b0b161024a0345635573d0a51
shafrinakhosyaqolba <100253621+shafrinakhosyaqolba@users.noreply.github.com>
2022-03-16 11:47:02 +0700

    Add files via upload

---
+++ proses.php
@@ -0,0 +1,17 @@
+<?php
+$proses = $_GET['proses.php'];
+$nama_lengkap = $_GET['nama'];
+$nilai_uts = $_GET['nilai_uts'];
+$nilai_uas = $_GET['nilai_uas'];
+$nilai_tugas = $_GET['nilai_tugas'];
+$mata_kuliah = $_GET['mata_kuliah'];
+
+   echo '<br/>Nama lengkap : '. $nama_lengkap;
+   echo '<br/>Nama UAS: '. $nilai_uas;
+   echo '<br/>Nilai UTS : '. $nilai_uts;
+   echo '<br/>Nilai Tugas/Praktikum : '. $nilai_tugas;
+   echo '<br/>Mata Kuliah : '. $mata_kuliah;
+
+switch ()
+   }
+?>
